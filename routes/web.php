401ac2c89ba406ee33089e229c36ede39ef1aecd
giovannitamburini <giovatambu@gmail.com>
2023-05-09 21:16:43 +0200

    added bonus

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $links = [
-        'characters',
-        'comics',
-        'movies',
-        'tv',
-        'games',
-        'collectibles',
-        'videos',
-        'fans',
-        'news',
-        'shop'
-    ];
-
     return view('home');
+});
+
+Route::get('/bonus', function () {
+
+    return view('bonus');
 });
 
 Route::get('/', function () {
